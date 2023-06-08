0751d6f67d1cc6eeca5b84018f33cb96c95481d5
Iris Young <irisdyoung@gmail.com>
2023-06-07 18:28:25 -0700

    Fix #56, correctly format phil helpstrings

--- sim_phil.py
+++ sim_phil.py
@@ -11,40 +11,40 @@ sim_view_phil_scope = phil.parse(
     detector_dist = 100
         .type = float
         .expert_level=1
-        .help = Distance in mm from sample to detector.
+        .help = "Distance in mm from sample to detector."
     mosaic_domains_bragg = 100
         .type = int
         .expert_level=1
-        .help = Number of rotation matrices to model in each simulation
-        .help = for Bragg peaks.
+        .help = "Number of rotation matrices to model in each simulation"
+                "for Bragg peaks."
     mosaic_domains_diffuse = 10
         .type = int
         .expert_level=1
-        .help = Number of domains to model for diffuse scattering only.
+        .help = "Number of domains to model for diffuse scattering only."
     oversampling = 1
         .type = int
         .expert_level=1
-        .help = Oversampling of the simulated diffraction in case of e.g.
-        .help = very large domain size and wide mosaic angles.
+        .help = "Oversampling of the simulated diffraction in case of e.g."
+                "very large domain size and wide mosaic angles."
     oscillation_n_steps = 10
         .type = int
         .expert_level=1
-        .help = Number of steps to sample in a single oscillation.
-        .help = (This number of simulations will be summed to produce a
-        .help = simulated rotation image.)
+        .help = "Number of steps to sample in a single oscillation."
+                "(This number of simulations will be summed to produce a"
+                "simulated rotation image.)"
     spectrum_shape = SASE (XFEL)
         .type = str
         .expert_level=1
-        .help = Enable overriding default spectrum shape to monochromatic for
-        .help = speed, critical when not using GPU, recommended when using
-        .help = diffuse scattering.
+        .help = "Enable overriding default spectrum shape to monochromatic for"
+                "speed, critical when not using GPU, recommended when using"
+                "diffuse scattering."
     context = *kokkos cuda cpu
         .type = choice
         .expert_level=3
-        .help = Backend for parallel execution.
+        .help = "Backend for parallel execution."
     high_visibility = False
         .type = bool
         .expert_level=0
-        .help = Increase font size and reduce panel width.
+        .help = "Increase font size and reduce panel width."
     """, process_includes=True)
 
